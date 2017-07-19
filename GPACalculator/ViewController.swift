fb55d3a926625b0f890126729addb048221a70fd
Xander Moffatt <xmoffatt@instructure.com>
2017-07-18 20:02:56 -0600

    initial images and all that

--- GPACalculator/ViewController.swift
+++ GPACalculator/ViewController.swift
@@ -8,18 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
